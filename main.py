0735ddbda4b6e803bbe50abf4d01df3747cb53c0
Kai Fabian <kai.fabian@student.hpi.uni-potsdam.de>
2011-10-25 22:36:06 +0200

    Updated main.py and created __main__.py to comply to Python module executable conventions (ref. http://docs.python.org/release/2.7/using/cmdline.html)

--- main.py
+++ main.py
@@ -3,11 +3,14 @@
 from PyQt4 import QtCore, QtGui
 from gen.mainwindow import Ui_MainWindow 
 
+def executeApplication():
+	import sys
+	app = QtGui.QApplication(sys.argv)
+	MainWindow = QtGui.QMainWindow()
+	ui = Ui_MainWindow()
+	ui.setupUi(MainWindow)
+	MainWindow.show()
+	sys.exit(app.exec_())
+
 if __name__ == "__main__":
-    import sys
-    app = QtGui.QApplication(sys.argv)
-    MainWindow = QtGui.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+	executeApplication()
