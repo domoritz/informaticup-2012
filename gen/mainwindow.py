3700208b88c358d06e9ce778209b31118acb757d
Dominik Moritz <domoritz@gmail.com>
2011-10-31 15:59:39 +0100

    main.py für mac gefixt (sys eins höher) und mainwindow neu gebaut

--- gen/mainwindow.py
+++ gen/mainwindow.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'MainWindow.ui'
+# Form implementation generated from reading ui file '../gui/mainwindow.ui'
 #
-# Created: Tue Oct 25 14:13:02 2011
+# Created: Mon Oct 31 15:53:04 2011
 #      by: PyQt4 UI code generator 4.8.5
 #
 # WARNING! All changes made in this file will be lost!
@@ -51,3 +51,4 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         pass
+
