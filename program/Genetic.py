2da69ef6506845d967d643c340c93be8191234db
Dominik Moritz <domoritz@gmail.com>
2011-11-04 14:56:34 +0100

    genetischer algo kann nun anfangslösung generieren

--- program/Genetic.py
+++ program/Genetic.py
@@ -1,42 +1,47 @@
 from data.DataInstance import DataInstance
 from program.Algorithm import Algorithm
 import random
+import pprint
 
 class Genetic(Algorithm):
-	""" Genetic algorithm """
+    """ Genetic algorithm """
 
-	def __init__(self, data, options = None):
-		super(Genetic, self).__init__(data)
+    def __init__(self, data, options = None):
+        super(Genetic, self).__init__(data)
 
-		if options:
-			self.options = options
-		else:
-			options = {
-				"popsize":10,
-				"childrenGroup": 2,
-				"mutation": 3,
-				"maxGenerations": 100,
-				"seed": 1
-			}
-		self.population = []
-		random.seed(options['seed'])
+        if options is None:
+            self.options = {
+                "popsize":20,
+                "childrenGroup": 2,
+                "mutation": 3,
+                "maxGenerations": 100,
+                "seed": 1
+            }
+        else:
+            self.options = options
 
-	def generate(solution = None):
-		"""creates a solution and yields it as a list 
-		of numbers which stand for the shops
-		i.e. [4,6,2,3,1,5]
-		this means, solve is a generator
-		"""
-		if False:
-			yield None
+        self.population = []
+        random.seed(self.options["seed"])
 
-	def brew(self):
-		"""generate a population"""
-		numberCities = 10
-		self.population = [[random.randint(0,numberCities-y) for y in range(numberCities-1)] for x in range(options.popsize)]
+    def generate(self, solution = None):
+        """creates a solution and yields it as a list 
+        of numbers which stand for the shops
+        i.e. [4,6,2,3,1,5]
+        this means, solve is a generator
+        """
+        self.brew()
+        if False:
+            yield None   
 
-	def evaluate(self):
-		"""docstring for evaluate"""
-		pass
+    def brew(self):
+        """generate a population"""
+        numberCities = 10
+        self.population = [[random.randint(0,numberCities-y) for y in range(numberCities)] for x in range(self.options["popsize"])]
+        pprint.pprint(self.population)
+
+
+    def evaluate(self):
+        """docstring for evaluate"""
+        pass
 
 
