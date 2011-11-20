e39c6c0de006cf472ae907d3119aaba779bbc7a4
Matthias Springer <me@matthiasspringer.de>
2011-11-20 15:06:26 +0100

    Drawing simple cities works now.

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -1,4 +1,4 @@
-from PyQt4.QtCore import QString, Qt, SIGNAL, SLOT
+from PyQt4.QtCore import QString, Qt, SIGNAL, SLOT, QRectF, QPointF
 from PyQt4.QtGui import *
 from gen.ui_mainwindow import Ui_MainWindow
 
@@ -10,4 +10,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 		self.setUnifiedTitleAndToolBarOnMac(True)
 
 		self.setupUi(self)
+
+	def drawCities(self, positions, dataInstance):
+		scene = QGraphicsScene()
+
+		for cityIndex in range(0, len(positions)):
+			scene.addEllipse(QRectF(positions[cityIndex][0], positions[cityIndex][1], 25, 25))
+			cityDescription = scene.addText(str(cityIndex))
+			cityDescription.setPos(positions[cityIndex][0], positions[cityIndex][1])
+		
+		for city1 in range(0, len(dataInstance.originalDistances)):
+			for city2 in range(0, len(dataInstance.originalDistances)):
+				if dataInstance.originalDistances.getDistance(city1, city2) != None:
+					scene.addLine(positions[city1][0]+12, positions[city1][1]+12, positions[city2][0]+12, positions[city2][1]+12)
+
+		self.graphicsView.setScene(scene)
+		self.graphicsView.show()
 
