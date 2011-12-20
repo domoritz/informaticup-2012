f3a0790a82d367d185e6a3ab04d263bb53b7ffcd
Malte Swart <malte.swart@student.hpi.uni-potsdam.de>
2011-12-20 15:24:27 +0100

    run*: use long commandline options - so it is easy to understand

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python . -c -d -i sample_data/prices_more.txt sample_data/distances_more.txt 
+python . --commandline --debug --input sample_data/prices_more.txt sample_data/distances_more.txt 
