ad62cab31b1915bea98b51076c123a663461b792
Matthias Springer <me@matthiasspringer.de>
2012-01-12 13:32:29 +0100

    DataParser liest jetzt auch die Namen.

--- program/dataParser.py
+++ program/dataParser.py
@@ -24,7 +24,7 @@ class DataParser:
 				firstRow = False
 		return result
 
-	def readPricesFile(self, pricesFile):
+	def readPricesFile(self, pricesFile, aDataInstance):
 		"""Parses a prices file into a DataPrices data structure."""
 		r = csv.reader(pricesFile, delimiter=',')
 		result = DataPrices()
@@ -33,16 +33,29 @@ class DataParser:
 		rowCounter = 0
 		
 		for rowCounter,row in enumerate(r):
-			if rowCounter > 1:
+			if rowCounter == 1:
+				columnCounter = 0
+				for col in row:
+					if columnCounter > 0 and aDataInstance != None:
+						aDataInstance.storeIndexToName[columnCounter] = col
+
+					columnCounter += 1
+			elif rowCounter > 1:
+				if aDataInstance != None:
+					aDataInstance.itemIndexToName[rowCounter] = row[0]
+
 				result.data.append([self.getNumeric(num) for num in row[2:]])
 				result.itemQuantity.append(row[1])
 		result.removeQuantities()
 		return result
-		
+
 	def readInstance(self, pricesFile, distancesFile):
 		"""Parses a prices file and a distances file into a DataInstance data structure."""
 		result = DataInstance()
-		result.originalPrices =self.readPricesFile(pricesFile)
+		result.itemIndexToName = {}
+		result.storeIndexToName = {}
+
+		result.originalPrices = self.readPricesFile(pricesFile, result)
 		result.prices = copy.deepcopy(result.originalPrices).prepare()
 		result.originalDistances = self.readDistancesFile(distancesFile)
 		result.distances = copy.deepcopy(result.originalDistances).prepare()
