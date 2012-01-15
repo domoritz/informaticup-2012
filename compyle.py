79ebb38de145b5b4f48e29ffc21806a12cc11a88
Kai Fabian <kai.fabian@student.hpi.uni-potsdam.de>
2012-01-15 17:41:24 +0100

    ComPYler

---
+++ compyle.py
@@ -0,0 +1,35 @@
+import sys, os
+from glob import glob
+from py2exe.build_exe import py2exe
+from distutils.core import setup
+
+real_argv = sys.argv
+
+def all_files(path):
+	fileLists = dict()
+	for root, subFolders, files in os.walk(path):
+		for file in files:
+			if not root in fileLists:
+				fileLists[root] = list()
+			fileLists[root].append(os.path.join(root,file))
+	print(fileLists)
+	return fileLists.items()
+
+if sys.platform == "win32":
+	sys.argv = sys.argv[0:1] + [
+		"py2exe",
+		"--includes", "sip",
+		"--dist-dir", "distribution",
+		"--compressed",
+		"--bundle-files", "3",
+		"--optimize", "2",
+		"--dll-excludes", "MSVCP90.DLL",
+	] + sys.argv[1:]
+	setup(
+		console=[{"script": "__main__.py"}],
+		data_files = 
+			all_files("dist") +
+			all_files("assets") + 
+			all_files("documentation") +
+			all_files("sample_data")
+	)
